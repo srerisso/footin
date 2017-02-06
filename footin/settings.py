0b9bd35f9952f8af81e2b468164d07a4de97971c
erisso <works@erisso.com>
2017-02-06 13:11:11 +0100

    Added GeoDjango model and started creating models for footin project

--- footin/settings.py
+++ footin/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'footin.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'footin',
-        'USER': 'jose',
-        'PASSWORD': 'zitronen',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'footin',
+#         'USER': 'jose',
+#         'PASSWORD': 'zitronen',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.gis.db import models  #GeoDjango Model
 
 # Create your models here.
 
@@ -12,3 +12,19 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+
+# Models de Footin
+# class Track(models.Model):
+# 	created_date = models.DateTimefield('date created')
+# 	name = models.CharField(max_length=100)
+
+# class TrackSegment(models.Model):
+# 	track = models.ForeignKey(Track, on_delete=models.CASCADE)
+# 	name = models.CharField(max_length=200)
+
+# class TrackWaypoints(models.Model):
+# 	segment = models.ForeignKey(TrackSegment, on_delete=models.CASCADE)
+# 	latitude =
+# 	longitude =
+# 	elevation = 
+# 	created = models.DateTimefield('ping gps')
