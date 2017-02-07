8afa86d36485e597dd33b18bba7e04bfe87463f1
erisso <works@erisso.com>
2017-02-07 17:41:18 +0100

    Added gitignore, and gpxpy library

---
+++ gpx/gpxpy-0.8.8/footin_test.py
@@ -0,0 +1,22 @@
+import gpxpy
+import gpxpy.gpx
+
+# Parsing an existing file:
+# -------------------------
+
+gpx_file = open('../data/2016-01-07-1354.gpx', 'r')
+
+gpx = gpxpy.parse(gpx_file)
+
+for track in gpx.tracks:
+	for segment in track.segments:
+		for point in segment.points:
+			print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
+
+for waypoint in gpx.waypoints:
+	print 'waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude)
+
+for route in gpx.routes:
+	print 'Route:'
+for point in route:
+	print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
